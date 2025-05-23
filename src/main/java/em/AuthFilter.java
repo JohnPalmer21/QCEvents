5203bc212e838ddd56383e2dbc4ebc61622c9546
Adityavdwiv <aditya.dwivedi78@qmail.cuny.edu>
2025-05-22 22:29:48 -0400

    update

--- src/main/java/em/AuthFilter.java
+++ src/main/java/em/AuthFilter.java
@@ -1,14 +1,19 @@
 package em;
 
+import java.io.IOException;
+
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.JwtException;
-
-import jakarta.servlet.*;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
 import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @WebFilter("/secure/*")
 public class AuthFilter implements Filter {
